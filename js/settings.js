55687f523bb8b1df7c8ae0daa85a85b971a762e7
zahid.khan <dead-dance@hotmail.com>
2023-05-24 15:30:00 +0600

    Upgrading

--- js/settings.js
+++ js/settings.js
@@ -1,17 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
     try {
-        
-        var $buythemediv = '<div class="buy-theme xs-display-none"><a href="https://1.envato.market/61ORG" target="_blank"><span>Purchase Theme</span></a></div>';
-        $('body').append($buythemediv);
-        
+
         /* ==========================================================================
          #PieChart For Skills Page
          ========================================================================== */
 
         $('.chart').easyPieChart({
             easing: 'easeOutBounce',
-            onStep: function (from, to, percent) {
+            onStep: function(from, to, percent) {
                 $(this.el).find('.percent').text(Math.round(percent));
             }
         });
@@ -31,20 +28,19 @@ $(document).ready(function () {
         /* ==========================================================================
          #Text Rotator 
          ========================================================================== */
-        $('#rotate').rotaterator({fadeSpeed: 800, pauseSpeed: 800});
+        $('#rotate').rotaterator({ fadeSpeed: 800, pauseSpeed: 800 });
 
         /* ==========================================================================
          #Orientation change event
          ========================================================================== */
-        $(window).on('orientationchange', function (event) {
+        $(window).on('orientationchange', function(event) {
             window.location.href = window.location.href;
         });
 
         //Videos
         $(".content-scroller").fitVids();
 
-    } catch (ex) {
-    }
+    } catch (ex) {}
 });
 
 /* ==========================================================================
@@ -60,14 +56,14 @@ progressBar(60, $('#progressBar3'));
  ========================================================================== */
 
 var $menu = $('#menu1'),
-        $menulink = $('.menu-link');
-$menulink.click(function () {
+    $menulink = $('.menu-link');
+$menulink.click(function() {
     $menulink.toggleClass('active');
     $menu.toggleClass('active');
     return false;
 });
 
-$('nav#menu1 a').click(function () {
+$('nav#menu1 a').click(function() {
     $('#menu1').removeClass('active');
 });
 
@@ -79,13 +75,9 @@ $('nav#menu1 a').click(function () {
 var iPad = navigator.userAgent.toLowerCase().indexOf("ipad");
 var iPhone = navigator.userAgent.toLowerCase().indexOf("iphone");
 var iPod = navigator.userAgent.toLowerCase().indexOf("ipod");
-if (iPad > -1 || iPhone > -1 || iPod > -1)
-{
-    window.onscroll = function () {
+if (iPad > -1 || iPhone > -1 || iPod > -1) {
+    window.onscroll = function() {
         $('.totop-link').css('position', 'absolute');
         $('.totop-link').css('top', (window.pageYOffset + window.innerHeight - 39) + 'px');
     };
 }
-
-
-
